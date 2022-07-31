3deaa0292055572ae13e9cacca16e30e4e286cf7
BoehmannFranz <franz.boehmann@gmail.com>
2022-07-31 11:13:37 +0200

    # 1) Selenium zum laufen kriegen (Chromedriver zum laufen kreigen --> path angeben wegen macos) # 2) Base nicht als hauptvariable entfernen (done) # 3) docker zum laufen kriegen (done) # 3) Grundfunktionen schreiben

--- rosa.functions.py
+++ rosa.functions.py
@@ -1,4 +1,4 @@
-import time  
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,13 +8,14 @@ from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.ChromeOptions() 
 options.add_argument("--incognito")
 
-# start webdriver
+# start webdriver 
 driver = webdriver.Chrome(options=options)
-
-# paste url
-driver.get("https://www.google.com/search?q=youtube+.boehmann+leuchten&oq=youtube+.boehmann+leuchten&aqs=chrome..69i57j69i64.9854j0j7&sourceid=chrome&ie=UTF-8#:~:text=.Boehmann%20%2D%20Leuchten%20%5BGlow,com%20%E2%80%BA%20watch")
+# # paste url
+driver.get("www.google.com/search?q=youtube+.boehmann+leuchten&oq=youtube+.boehmann+leuchten&aqs=chrome..69i57j69i64.9854j0j7&sourceid=chrome&ie=UTF-8#:~:text=.Boehmann%20%2D%20Leuchten%20%5BGlow,com%20%E2%80%BA%20watch")
+print(driver.title)
 
 #
-# 1) Selenium zum laufen kriegen
-# 2) Base nicht als hauptvariable entfernen
+# 1) Selenium zum laufen kriegen (Chromedriver zum laufen kreigen --> path angeben wegen macos)
+# 2) Base nicht als hauptvariable entfernen (done)
+# 3) docker zum laufen kriegen (done)
 # 3) Grundfunktionen schreiben 
